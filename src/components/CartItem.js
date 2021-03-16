57f4ad283065724e269cc56d358602c12ff82a57
iamcristos <nmereginivincent@gmail.com>
2021-03-16 09:40:00 +0100

    added no item in cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { CartCard, Btn, ControlDiv, CartImage, CartContent } from './Styles';
 
-function CartItem({carts, removeCart, addCart, deleteCart}) {
+function CartItem({carts, removeCart, addCart, deleteCart, currencyValue}) {
     return (
         <>
-        {carts.length && carts?.map(cart => (
+        {carts.length ? carts?.map(cart => (
         <CartCard key={cart.id}>
             <div style={{ display: 'flex', justifyContent: 'space-between', padding: '5px'}}>
                 <p>{cart.title}</p>
@@ -16,12 +16,11 @@ function CartItem({carts, removeCart, addCart, deleteCart}) {
                     <p>{cart.count}</p>
                     <Btn onClick={() => addCart(cart)} data-testid="addCount">+</Btn>
                 </ControlDiv>
-                <p>{cart.price}</p>
+                <p>{cart.price} {currencyValue}</p>
                 <CartImage src={cart.image_url}  alt={cart.title}/>
             </CartContent>
         </CartCard>
-
-        ))}
+        )) : <h1> No Item In the cart</h1> }
         </>
     )
 }
